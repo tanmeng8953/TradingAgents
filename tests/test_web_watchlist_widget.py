440ae16af5c381055df66f2897b92166066c8870
tanmeng8953 <tanmeng8953@gmail.com>
2026-06-18 15:45:10 +0800

    Clarify research watchlist crawler UI

--- tests/test_web_watchlist_widget.py
+++ tests/test_web_watchlist_widget.py
@@ -10,4 +10,6 @@ def test_static_console_embeds_research_only_watchlist_widget():
 
     assert "自选股票实时价格" in html
     assert "research_only" in html
+    assert "可增加/删除自选股" in html
+    assert "点击价格行查看走势图" in html
     assert "http://127.0.0.1:8765/workbench/crawler-widget/trading" in html
