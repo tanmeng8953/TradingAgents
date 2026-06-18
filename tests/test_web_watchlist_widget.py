bd89c4143be65339b87dd9ecf32ad0aa1ea5861f
tanmeng8953 <tanmeng8953@gmail.com>
2026-06-18 18:39:15 +0800

    Increase watchlist widget height

--- tests/test_web_watchlist_widget.py
+++ tests/test_web_watchlist_widget.py
@@ -12,5 +12,5 @@ def test_static_console_embeds_research_only_watchlist_widget():
     assert "research_only" in html
     assert "可增加/删除自选股" in html
     assert "点击价格行查看走势图" in html
-    assert "height: 620px" in html
+    assert "height: 760px" in html
     assert "http://127.0.0.1:8765/workbench/crawler-widget/trading" in html
