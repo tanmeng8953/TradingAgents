d2a4b48267d9cfb6994358087edfc71095c2acb6
tanmeng8953 <tanmeng8953@gmail.com>
2026-06-18 21:55:26 +0800

    Describe search-based watchlist edits

--- tests/test_web_watchlist_widget.py
+++ tests/test_web_watchlist_widget.py
@@ -11,6 +11,8 @@ def test_static_console_embeds_research_only_watchlist_widget():
     assert "自选股票实时价格" in html
     assert "research_only" in html
     assert "每只股票下方直接显示自己的 K线图" in html
-    assert "按钮增加/删除自选股" in html
+    assert "搜索栏输入股票代码或数字" in html
+    assert "添加自选" in html
+    assert "删除自选" in html
     assert "height: 1080px" in html
     assert "http://127.0.0.1:8765/workbench/crawler-widget/trading" in html
